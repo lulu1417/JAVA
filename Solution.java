d8f655acf35a84c26573109c610e59fc0233347f
lulu1417 <43202244+lulu1417@users.noreply.github.com>
2019-04-26 11:43:24 +0800

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,17 @@
+
+public class Solution {
+	 public int[] twoSum(int[] nums, int target) {
+		 int[] ans= {0,0};
+		 for(int i=0; i<nums.length;i++) {
+	    	 for(int j=i+1;j<nums.length;j++) {
+	    		 if(nums[i]+nums[j]==target) {
+	    			ans[0] = nums[i]; 
+	    			ans[1] = nums[i];
+	    			return ans;
+	    		 }
+	    	 }
+	    } 
+		 return ans;
+	  }
+
+}
