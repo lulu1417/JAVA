829556a03d4e15f56f42125105d910e417cabb72
lulu1417 <43202244+lulu1417@users.noreply.github.com>
2019-06-05 18:38:07 +0800

    Add files via upload

---
+++ Lab/Polymorphism/Animal/Cat.java
@@ -0,0 +1,6 @@
+
+public class Cat extends Animal {
+	public void run() {
+		System.out.println("THe cat is running");
+	}
+}
---
+++ Lab/Polymorphism/Animal/Dog.java
@@ -0,0 +1,6 @@
+
+public class Dog extends Animal {
+	public void run() {
+		System.out.println("The dog is running");
+	}
+}
