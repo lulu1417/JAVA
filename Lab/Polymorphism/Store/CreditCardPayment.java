4a5a18f66197a8c1f78974a3af9c86b27ff1e73f
lulu1417 <43202244+lulu1417@users.noreply.github.com>
2019-06-05 18:41:01 +0800

    Add files via upload

---
+++ Lab/Polymorphism/Store/CreditCardPayment.java
@@ -0,0 +1,10 @@
+
+public class CreditCardPayment extends Payment{
+	public void pay() { //like rewrite pay()
+		System.out.println("Pay with credit card");
+	}
+	public void sign(){
+		System.out.println("Signing...");
+	}
+
+}
---
+++ Lab/Polymorphism/Store/Payment.java
@@ -0,0 +1,11 @@
+
+public class Payment {
+	public void pay() {
+		System.out.println("Pay in cash");
+	}
+	public void checkout() {
+		pay();
+		
+	}
+}
+
